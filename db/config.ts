746f018daa03ed6bbc07c4ca1eda4e2378225d4f
jhon-sebastian-rodriguez-bohada-23 <rodriguezbohadajhonsebastian23@gmail.com>
2025-03-25 21:58:27 -0500

    add tables db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,25 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column} from 'astro:db';
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    id: column.number({primaryKey: true}),
+    googleId: column.text({ unique: true }),
+    name: column.text()
+  }
+});
+
+const Report = defineTable({
+  columns: {
+    reportId: column.number(),
+    UserId: column.number({references: () => User.columns.id}),
+    title: column.text(),
+    location: column.text(),
+    description: column.text(),
+    image: column.text(),
+    state: column.text()
+  }
+});
+
 export default defineDb({
-  tables: {}
+  tables: {User, Report}
 });
