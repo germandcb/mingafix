db8371e00f0f1784b1f5e5f03ff0f179e3eb6142
germandcb <germancelis16@gmail.com>
2025-04-20 16:27:30 -0500

    Implementar gestión de autenticación en el Navbar, actualizar middleware para manejar el estado de inicio de sesión y redirigir usuarios no autenticados en el dashboard; eliminar página de registro y definir tipos de usuario en el entorno.

---
+++ src/env.d.ts
@@ -0,0 +1,16 @@
+/// <reference path="../types/global.d.ts" />
+/// <reference types="astro/client" />
+
+interface User { 
+    email: string;
+    name: string;
+    avatar: string;
+    emailVerified: boolean;
+}
+
+declare namespace App { 
+    interface Locals { 
+        isLoggedIn: boolean;
+        user: User | null;
+    }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,24 @@
 import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
+import { firebase } from "./firebase/config";
 
 const privateRoutes = ["/dashboard"];
 
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware(({ url, request }, next) => {
+export const onRequest = defineMiddleware(({ url, request, locals }, next) => {
+    const isLoggedIn = !!firebase.auth.currentUser;
+    const user = firebase.auth.currentUser;
 
-    const authHeaders = request.headers.get('authorization') ?? '';
-
-
-
-    if (privateRoutes.includes(url.pathname)) {
-
-        if (privateRoutes.includes(url.pathname)) {
-            checkLocalAuth(authHeaders, next);
-        }
+    locals.isLoggedIn = isLoggedIn;
+    
+    if (user) { 
+        locals.user = {
+            email: user.email!,
+            name: user.displayName!,
+            avatar: user.photoURL ?? '',
+            emailVerified: user.emailVerified,
+        };
     }
+
     return next();
 });
-
-const checkLocalAuth = (authHeaders: string, next: MiddlewareNext) => {
-
-    if (!authHeaders) {
-
-        const authValue = authHeaders.split(' ').at(-1) ?? 'user:pass';
-        const decodedValude = atob(authValue).split(':');
-        const [user, password] = decodedValude;
-
-        if (user === 'admin' && password === 'admin') {
-            return next();
-        }
-    }
-
-    return new Response( 'Auth Necesaria', { 
-        status: 401,
-        headers: {
-            'WWW-Authenticate': 'Basic real="Secure Area"'
-        }
-    });
-}
